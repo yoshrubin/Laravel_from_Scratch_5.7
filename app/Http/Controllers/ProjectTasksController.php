8876931b85bb09d5f0a3a556790a798017b5dcd6
yoshirubin <yoshrubin@gmail.com>
2018-12-04 16:25:48 +0200

    Episode 19

--- app/Http/Controllers/ProjectTasksController.php
+++ app/Http/Controllers/ProjectTasksController.php
@@ -20,9 +20,11 @@ class ProjectTasksController extends Controller
 
     public function update(Task $task)
     {
-        $task->update([
-            'completed' => request()->has('completed')
-        ]);
+        // $task->update([
+        //     'completed' => request()->has('completed')
+        // ]);
+
+        request()->has('completed') ? $task->complete() : $task->incomplete();
 
         return back();
     }
--- app/Task.php
+++ app/Task.php
@@ -6,12 +6,20 @@ use Illuminate\Database\Eloquent\Model;
 
 class Task extends Model
 {
-    protected $fillable = [
-        'project_id', 'description', 'completed'
-    ];
+    protected $guarded = [];
 
     public function project()
     {
         return $this->belongsTo(Project::class);
+    }
+
+    public function complete($completed = true)
+    {
+        $this->update(compact('completed'));
+    }
+
+    public function incomplete()
+    {
+        $this->complete(false);
     }
 }
